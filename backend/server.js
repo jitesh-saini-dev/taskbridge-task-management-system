44e8e6836fd8420d9499bd58847f13acaf41ccff
jitesh <iamjiteshsaini2004@gmail.com>
2026-08-10 04:21:11 +0530

    Remove unnecessary .gitignore files from backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -4,23 +4,25 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 const taskRoutes = require("./routes/taskRoutes");
+
 app.use("/", taskRoutes);
 
-// MongoDB Connection
-const mongoURI = process.env.MONGODB_URI;
-
 mongoose
-  .connect(mongoURI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((error) => console.log("MongoDB Error:", error.message));
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((error) => {
+    console.log("MongoDB Error:", error.message);
+  });
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`server is running on port ${PORT}`);
